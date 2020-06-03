d86c918b6f7612a7769b5dc737071c7aecb93946
Valerio Santinelli <santinelli@altralogica.it>
2020-06-03 11:52:44 +0200

    Spatial hash example updated

--- spatialhash/src/main.c
+++ spatialhash/src/main.c
@@ -34,22 +34,22 @@
 //#define HITBOX_ENEMY
 #define CIRCLE_ENEMY
 
-binocle_window window;
+binocle_window *window;
 binocle_input input;
-binocle_viewport_adapter adapter;
+binocle_viewport_adapter *adapter;
 binocle_camera camera;
-binocle_sprite player;
+binocle_sprite *player;
 kmVec2 player_pos;
 binocle_gd gd;
 binocle_bitmapfont *font;
-binocle_image font_image;
-binocle_texture font_texture;
-binocle_material font_material;
-binocle_sprite font_sprite;
+binocle_image *font_image;
+binocle_texture *font_texture;
+binocle_material *font_material;
+binocle_sprite *font_sprite;
 kmVec2 font_sprite_pos;
 binocle_collider_hitbox player_hitbox;
 binocle_collider_hitbox enemy_hitbox;
-binocle_sprite enemy;
+binocle_sprite *enemy;
 kmVec2 enemy_pos;
 binocle_collider_circle player_hitbox_c;
 binocle_collider_circle enemy_hitbox_c;
@@ -60,46 +60,46 @@ binocle_collider enemy_collider;
 binocle_collider_ptr_array_t colliding_colliders;
 
 void main_loop() {
-  binocle_window_begin_frame(&window);
+  binocle_window_begin_frame(window);
   binocle_input_update(&input);
 
   if (input.resized) {
-    kmVec2 oldWindowSize = {.x = window.width, .y = window.height};
-    window.width = input.newWindowSize.x;
-    window.height = input.newWindowSize.y;
-    binocle_viewport_adapter_reset(&adapter, oldWindowSize, input.newWindowSize);
+    kmVec2 oldWindowSize = {.x = window->width, .y = window->height};
+    window->width = input.newWindowSize.x;
+    window->height = input.newWindowSize.y;
+    binocle_viewport_adapter_reset(adapter, oldWindowSize, input.newWindowSize);
     input.resized = false;
   }
 
 
   if (binocle_input_is_key_pressed(&input, KEY_RIGHT)) {
-    player_pos.x += 50 * (1.0/window.frame_time);
+    player_pos.x += 50 * (1.0/window->frame_time);
   } else if (binocle_input_is_key_pressed(&input, KEY_LEFT)) {
-    player_pos.x -= 50 * (1.0/window.frame_time);
+    player_pos.x -= 50 * (1.0/window->frame_time);
   }
 
   if (binocle_input_is_key_pressed(&input, KEY_UP)) {
-    player_pos.y += 50 * (1.0/window.frame_time);
+    player_pos.y += 50 * (1.0/window->frame_time);
   } else if (binocle_input_is_key_pressed(&input, KEY_DOWN)) {
-    player_pos.y -= 50 * (1.0/window.frame_time);
+    player_pos.y -= 50 * (1.0/window->frame_time);
   }
 
   player_hitbox.aabb.min.x = player_pos.x;
   player_hitbox.aabb.min.y = player_pos.y;
-  player_hitbox.aabb.max.x = player_pos.x + player.material->texture->width;
-  player_hitbox.aabb.max.y = player_pos.y + player.material->texture->height;
+  player_hitbox.aabb.max.x = player_pos.x + player->material->albedo_texture->width;
+  player_hitbox.aabb.max.y = player_pos.y + player->material->albedo_texture->height;
 
-  player_hitbox_c.center.x = player_pos.x + (float)player.material->texture->width / 2.0f;
-  player_hitbox_c.center.y = player_pos.y + (float)player.material->texture->height / 2.0f;
+  player_hitbox_c.center.x = player_pos.x + (float)player->material->albedo_texture->width / 2.0f;
+  player_hitbox_c.center.y = player_pos.y + (float)player->material->albedo_texture->height / 2.0f;
 
-  binocle_window_clear(&window);
+  binocle_window_clear(window);
   kmVec2 scale;
   scale.x = 1.0f;
   scale.y = 1.0f;
   kmMat4 view_matrix;
   kmMat4Identity(&view_matrix);
-  binocle_sprite_draw(&player, &gd, (uint64_t)player_pos.x, (uint64_t)player_pos.y, &adapter.viewport, 0, &scale, &camera);
-  binocle_sprite_draw(&enemy, &gd, (uint64_t)enemy_pos.x, (uint64_t)enemy_pos.y, &adapter.viewport, 0, &scale, &camera);
+  binocle_sprite_draw(player, &gd, (uint64_t)player_pos.x, (uint64_t)player_pos.y, &adapter->viewport, 0, &scale, &camera);
+  binocle_sprite_draw(enemy, &gd, (uint64_t)enemy_pos.x, (uint64_t)enemy_pos.y, &adapter->viewport, 0, &scale, &camera);
 
 #if defined(HITBOX_PLAYER) && defined(HITBOX_ENEMY)
   bool collides = binocle_collide_hitbox_to_hitbox(&player_hitbox, &enemy_hitbox);
@@ -122,8 +122,8 @@ void main_loop() {
     collides = binocle_collide_circle_to_circle(&player_hitbox_c, &enemy_hitbox_c);
   }
 
-  binocle_gd_draw_circle(&gd, player_hitbox_c.center, player_hitbox_c.radius, collides ? binocle_color_red_translucent() : binocle_color_green_translucent(), adapter.viewport, view_matrix);
-  binocle_gd_draw_circle(&gd, enemy_hitbox_c.center, enemy_hitbox_c.radius, collides ? binocle_color_red_translucent() : binocle_color_green_translucent(), adapter.viewport, view_matrix);
+  binocle_gd_draw_circle(&gd, player_hitbox_c.center, player_hitbox_c.radius, collides ? binocle_color_red_translucent() : binocle_color_green_translucent(), adapter->viewport, view_matrix);
+  binocle_gd_draw_circle(&gd, enemy_hitbox_c.center, enemy_hitbox_c.radius, collides ? binocle_color_red_translucent() : binocle_color_green_translucent(), adapter->viewport, view_matrix);
 #endif
 
   /*
@@ -135,10 +135,10 @@ void main_loop() {
   end.y = 240.0f/2;
   binocle_gd_draw_line(&gd, start, end, binocle_color_red(), adapter.viewport, view_matrix);
    */
-  binocle_bitmapfont_draw_string(font, "TEST", 12, &gd, 20, 20, adapter.viewport, binocle_color_white(), view_matrix);
+  binocle_bitmapfont_draw_string(font, "TEST", 12, &gd, 20, 20, adapter->viewport, binocle_color_white(), view_matrix);
   //binocle_sprite_draw(font_sprite, &gd, (uint64_t)font_sprite_pos.x, (uint64_t)font_sprite_pos.y, adapter.viewport);
-  binocle_window_refresh(&window);
-  binocle_window_end_frame(&window);
+  binocle_window_refresh(window);
+  binocle_window_end_frame(window);
   //binocle_log_info("FPS: %d", binocle_window_get_fps(&window));
 }
 
@@ -146,56 +146,56 @@ int main(int argc, char *argv[])
 {
   binocle_sdl_init();
   window = binocle_window_new(320, 240, "Binocle Test Game");
-  binocle_window_set_background_color(&window, binocle_color_azure());
-  adapter = binocle_viewport_adapter_new(window, BINOCLE_VIEWPORT_ADAPTER_KIND_SCALING, BINOCLE_VIEWPORT_ADAPTER_SCALING_TYPE_PIXEL_PERFECT, window.original_width, window.original_height, window.original_width, window.original_height);
-  camera = binocle_camera_new(&adapter);
+  binocle_window_set_background_color(window, binocle_color_azure());
+  adapter = binocle_viewport_adapter_new(window, BINOCLE_VIEWPORT_ADAPTER_KIND_SCALING, BINOCLE_VIEWPORT_ADAPTER_SCALING_TYPE_PIXEL_PERFECT, window->original_width, window->original_height, window->original_width, window->original_height);
+  camera = binocle_camera_new(adapter);
   input = binocle_input_new();
   binocle_shader_init_defaults();
   char filename[1024];
   sprintf(filename, "%s%s", BINOCLE_DATA_DIR, "wabbit_alpha.png");
-  binocle_image image = binocle_image_load(filename);
-  binocle_texture texture = binocle_texture_from_image(image);
+  binocle_image *image = binocle_image_load(filename);
+  binocle_texture *texture = binocle_texture_from_image(image);
   char vert[1024];
   sprintf(vert, "%s%s", BINOCLE_DATA_DIR, "default.vert");
   char frag[1024];
   sprintf(frag, "%s%s", BINOCLE_DATA_DIR, "default.frag");
-  binocle_shader shader = binocle_shader_load_from_file(vert, frag);
-  binocle_material material = binocle_material_new();
-  material.texture = &texture;
-  material.shader = &shader;
-  player = binocle_sprite_from_material(&material);
+  binocle_shader *shader = binocle_shader_load_from_file(vert, frag);
+  binocle_material *material = binocle_material_new();
+  material->albedo_texture = texture;
+  material->shader = shader;
+  player = binocle_sprite_from_material(material);
   player_pos.x = 50;
   player_pos.y = 50;
   kmAABB2 player_aabb2;
   player_aabb2.min.x = player_pos.x;
   player_aabb2.min.y = player_pos.y;
-  player_aabb2.max.x = player_pos.x + texture.width;
-  player_aabb2.max.y = player_pos.y + texture.height;
+  player_aabb2.max.x = player_pos.x + texture->width;
+  player_aabb2.max.y = player_pos.y + texture->height;
   player_hitbox = binocle_collider_hitbox_new(player_aabb2);
   kmVec2 player_center;
-  player_center.x = player_pos.x + (float)texture.width / 2;
-  player_center.y = player_pos.y + (float)texture.height / 2;
-  player_hitbox_c = binocle_collider_circle_new(texture.width > texture.height ? (texture.width / 2) : (texture.height / 2), player_center);
+  player_center.x = player_pos.x + (float)texture->width / 2;
+  player_center.y = player_pos.y + (float)texture->height / 2;
+  player_hitbox_c = binocle_collider_circle_new(texture->width > texture->height ? (texture->width / 2.0f) : (texture->height / 2.0f), player_center);
 
   sprintf(filename, "%s%s", BINOCLE_DATA_DIR, "enemy.png");
-  binocle_image enemy_image = binocle_image_load(filename);
-  binocle_texture enemy_texture = binocle_texture_from_image(enemy_image);
-  binocle_material enemy_material = binocle_material_new();
-  enemy_material.texture = &enemy_texture;
-  enemy_material.shader = &shader;
-  enemy = binocle_sprite_from_material(&enemy_material);
+  binocle_image *enemy_image = binocle_image_load(filename);
+  binocle_texture *enemy_texture = binocle_texture_from_image(enemy_image);
+  binocle_material *enemy_material = binocle_material_new();
+  enemy_material->albedo_texture = enemy_texture;
+  enemy_material->shader = shader;
+  enemy = binocle_sprite_from_material(enemy_material);
   enemy_pos.x = 100;
   enemy_pos.y = 50;
   kmAABB2 enemy_aabb2;
   enemy_aabb2.min.x = enemy_pos.x;
   enemy_aabb2.min.y = enemy_pos.y;
-  enemy_aabb2.max.x = enemy_pos.x + enemy_texture.width;
-  enemy_aabb2.max.y = enemy_pos.y + enemy_texture.height;
+  enemy_aabb2.max.x = enemy_pos.x + enemy_texture->width;
+  enemy_aabb2.max.y = enemy_pos.y + enemy_texture->height;
   enemy_hitbox = binocle_collider_hitbox_new(enemy_aabb2);
   kmVec2 enemy_center;
-  enemy_center.x = enemy_pos.x + (float)enemy_texture.width / 2;
-  enemy_center.y = enemy_pos.y + (float)enemy_texture.height / 2;
-  enemy_hitbox_c = binocle_collider_circle_new(enemy_texture.width > enemy_texture.height ? (enemy_texture.width / 2) : (enemy_texture.height / 2), enemy_center);
+  enemy_center.x = enemy_pos.x + (float)enemy_texture->width / 2;
+  enemy_center.y = enemy_pos.y + (float)enemy_texture->height / 2;
+  enemy_hitbox_c = binocle_collider_circle_new(enemy_texture->width > enemy_texture->height ? (enemy_texture->width / 2.0f) : (enemy_texture->height / 2.0f), enemy_center);
 
   spatial_hash = binocle_spatial_hash_new(1000, 1000, 32);
   player_collider = binocle_collider_new();
@@ -218,10 +218,10 @@ int main(int argc, char *argv[])
   font_image = binocle_image_load(font_image_filename);
   font_texture = binocle_texture_from_image(font_image);
   font_material = binocle_material_new();
-  font_material.texture = &font_texture;
-  font_material.shader = &shader;
-  font->material = &font_material;
-  font_sprite = binocle_sprite_from_material(&font_material);
+  font_material->albedo_texture = font_texture;
+  font_material->shader = shader;
+  font->material = font_material;
+  font_sprite = binocle_sprite_from_material(font_material);
   font_sprite_pos.x = 0;
   font_sprite_pos.y = -256;
 
@@ -239,6 +239,21 @@ int main(int argc, char *argv[])
 #endif
   binocle_log_info("Quit requested");
 #endif
+  binocle_sprite_destroy(font_sprite);
+  binocle_material_destroy(font_material);
+  binocle_texture_destroy(font_texture);
+  binocle_image_destroy(font_image);
+  binocle_sprite_destroy(enemy);
+  binocle_material_destroy(enemy_material);
+  binocle_texture_destroy(enemy_texture);
+  binocle_image_destroy(enemy_image);
+  binocle_sprite_destroy(player);
+  binocle_material_destroy(material);
+  binocle_shader_destroy(shader);
+  binocle_texture_destroy(texture);
+  binocle_image_destroy(image);
+  binocle_viewport_adapter_destroy(adapter);
+  binocle_window_destroy(window);
   binocle_sdl_exit();
 }
 
